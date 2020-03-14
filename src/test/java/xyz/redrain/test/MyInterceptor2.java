4b830adf2eb32616a09205f7cd6b766f30ef9c61
RedRain <liangmin9@163.com>
2020-03-14 17:59:31 +0800

    兼容无下划线模式

---
+++ src/test/java/xyz/redrain/test/MyInterceptor2.java
@@ -0,0 +1,46 @@
+package xyz.redrain.test;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+/**
+ * Created by RedRain on 2020/3/14.
+ *
+ * @author RedRain
+ * @version 1.0
+ */
+
+/**
+ * 执行器Executor（update、query、commit、rollback等方法）；
+ * 参数处理器ParameterHandler（getParameterObject、setParameters方法）；
+ * 结果集处理器ResultSetHandler（handleResultSets、handleOutputParameters等方法）；
+ * SQL语法构建器StatementHandler（prepare、parameterize、batch、update、query等方法）；
+ */
+@Intercepts(@Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class}))
+public class MyInterceptor2 implements Interceptor {
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+
+       // MetaObject metaObject = SystemMetaObject.forObject(invocation.getTarget());
+      //  Object value = metaObject.getValue("delegate.boundSql.sql");
+        System.out.println("intercept2");
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object target) {
+
+        //System.out.println("plugin");
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
